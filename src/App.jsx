ea335ff84b067eb85c235f856db1eef4096cceb0
Johan Swan <jswannieman@gmail.com>
2024-01-15 19:44:14 +1300

    Update App JSX Component

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,17 @@ import UserContextProvider from "./context/UserContextProvider";
 function App() {
   return (
     <UserContextProvider>
-      <h1>React Context API</h1>
-      <Login />
-      <Profile />
+      <div className="w-full  h-lvh bg-rose-100">
+        <h1 className="w-1/5 mx-auto bg-blue-950 text-white text-center p-2 mb-16">
+          React Context API
+        </h1>
+        <div className="flex flex-wrap justify-center items-center mx-auto mb-10">
+          <Login />
+        </div>
+        <div className="flex flex-wrap justify-center items-center mx-auto">
+          <Profile />
+        </div>
+      </div>
     </UserContextProvider>
   );
 }
