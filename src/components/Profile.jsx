4b187807fe3be10c5fab026ac341b9c2e7027397
Johan Swan <jswannieman@gmail.com>
2024-01-18 17:04:22 +1300

    Update Profile JSX Component

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -4,11 +4,14 @@ import UserContext from "../context/UserContext";
 function Profile() {
   const { user } = useContext(UserContext);
 
-  if (!user) return <h1>Not logged in </h1>;
+  if (!user) return <h1>Not logged in</h1>;
+
   if (user.username.length < 5)
     return <h1>User Name must be at least 5 characters</h1>;
+
   if (user.password.length < 8)
     return <h1>Password must be at least 8 characters</h1>;
+
   return (
     <div>
       <h1>Profile : {user.username}</h1>
